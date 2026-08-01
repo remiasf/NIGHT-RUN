34f357edd10d3fd3af0116c156fc6234190b04b0
Dyshliuk Dmytro <koiiimap225@gmail.com>
2026-08-01 17:19:08 +0300

    remove: e2e tests

--- src/auth/firebase-auth/firebase-auth.guard.spec.ts
+++
@@ -1,7 +0,0 @@
-import { FirebaseAuthGuard } from './firebase-auth.guard';
-
-describe('FirebaseAuthGuard', () => {
-  it('should be defined', () => {
-    expect(new FirebaseAuthGuard()).toBeDefined();
-  });
-});
